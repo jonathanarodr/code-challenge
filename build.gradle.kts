8925c050d7e1a1d9fd0de59a731e98c32bd9202d
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2024-09-18 22:16:38 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "br.com.jonathan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/util/AssertEquals.kt
@@ -0,0 +1,22 @@
+package br.com.jonathan.util
+
+private const val GreenColor = "\u001B[32m"
+private const val RedColor = "\u001B[31m"
+private const val Reset = "\u001B[0m"
+private const val FontBold = "\u001B[1m"
+
+object AssertEquals {
+
+    infix fun Any.equalsTo(expected: Any) {
+        if (this != expected) {
+            println("$FontBold$RedColor[✖]$Reset Failure\n")
+            error(
+                "Result value is not the same as expected\n" +
+                "Result:   $this\n" +
+                "Expected: $expected"
+            )
+        } else {
+            println("\n$FontBold$GreenColor[✓]$Reset Successful")
+        }
+    }
+}
