448afe4111f18fbc719118d9798c9152b03b67a7
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2025-11-03 22:31:10 -0300

    Review code challenge

--- src/main/kotlin/br/com/jonathanarodr/challenges/tests/TransactionBash.kt
+++ src/main/kotlin/br/com/jonathanarodr/challenges/tests/TransactionBash.kt
@@ -1,72 +1,51 @@
 package br.com.jonathanarodr.challenges.tests
-//
-//data class Transaction(
-//    val payee: String,
-//    val amount: Int,
-//    val payer: String,
-//)
-//
-//fun getTransactions() = listOf(
-//    Transaction("BoA", 132, "Chase"),
-//    Transaction("BoA", 827, "Chase"),
-//    Transaction("Wells Fargo", 751, "BoA"),
-//    Transaction("BoA", 585, "Chase"),
-//    Transaction("Chase", 877, "Wells Fargo"),
-//    Transaction("Wells Fargo", 157, "Chase"),
-//    Transaction("Wells Fargo", 904, "Chase"),
-//    Transaction("Chase", 548, "Wells Fargo"),
-//    Transaction("Chase", 976, "BoA"),
-//    Transaction("BoA", 872, "Wells Fargo"),
-//    Transaction("Wells Fargo", 571, "Chase"),
-//)
-//
-//fun main() {
-//    val transactions = getTransactions()
-//    val batch = mutableMapOf<String, Transaction>()
-//
-//    transactions.forEach { transaction ->
-//        val key = transaction.payee + transaction.payer
-//
-//        if (batch.containsKey(key)) {
-//            val amount = (batch[key]?.amount ?: 0) + transaction.amount
-//            batch[key] = transaction.copy(amount = amount)
-//        } else {
-//            batch[key] = transaction
-//        }
-//    }
-//
-//    println("Print batch...")
-//    batch.values.forEach { println(it) }
-//}
 
 /**
  * Given a set of transactions with payee, payer and payment amount
  * calculate the number of transactions if the payee or payer is the same and return a new list of transactions
  * containing the sum of the transaction amounts
  *
- * I.e.,
+ * i.e.
  *      Transaction1: { "BoA", 100, "Chase" }
  *      Transaction2: { "Chase", 140, "BoA" }
  *      BashTransaction: { "Chase", "BoA", 240 }
  */
 
 private data class Transaction(
-    val payer: String,
     val payee: String,
+    val payer: String,
     val amount: Int,
 )
-
-private data class TransactionBash(
-    val identifier: String,
-    val amount: Int,
-)
-
-private fun calculateTransactions() {
-
-}
 
 fun main() {
     val transactions = listOf(
-        Transaction("", "", 100),
+        Transaction(payee = "BoA", payer = "Chase", amount = 132),
+        Transaction(payee = "BoA", payer = "Chase", amount = 827),
+        Transaction(payee = "Wells Fargo", payer = "BoA", amount = 751),
+        Transaction(payee = "BoA", payer = "Chase", amount = 585),
+        Transaction(payee = "Chase", payer = "Wells Fargo", amount = 877),
+        Transaction(payee = "Wells Fargo", payer = "Chase", amount = 157),
+        Transaction(payee = "Wells Fargo", payer = "Chase", amount = 904),
+        Transaction(payee = "Chase", payer = "Wells Fargo", amount = 548),
+        Transaction(payee = "Chase", payer = "BoA", amount = 976),
+        Transaction(payee = "BoA", payer = "Wells Fargo", amount = 872),
+        Transaction(payee = "Wells Fargo", payer = "Chase", amount = 571),
     )
+
+    val bashTransactions = mutableMapOf<Pair<String, String>, Int>()
+
+    transactions.forEach { transaction ->
+        val payeeToPayer = transaction.payee to transaction.payer
+        val payerToPayee = transaction.payer to transaction.payee
+
+        val transactionId = when {
+            bashTransactions.contains(payeeToPayer) -> payeeToPayer
+            bashTransactions.contains(payerToPayee) -> payerToPayee
+            else -> payerToPayee
+        }
+
+        bashTransactions[transactionId] = (bashTransactions[transactionId] ?: 0) + transaction.amount
+    }
+
+    println("Result: $bashTransactions")
 }
