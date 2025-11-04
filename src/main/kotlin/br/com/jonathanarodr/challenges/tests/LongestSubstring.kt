d20929ba0b2b7b9cf05835c17f99b2a73ad43376
Jonathan Rodrigues <jonathan.a.rodr@gmail.com>
2025-11-03 22:31:18 -0300

    Review code challenge

--- src/main/kotlin/br/com/jonathanarodr/challenges/tests/LongestSubstring.kt
+++ src/main/kotlin/br/com/jonathanarodr/challenges/tests/LongestSubstring.kt
@@ -1,59 +1,25 @@
 package br.com.jonathanarodr.challenges.tests
 
-private fun longestSubstringLow(input: String): Int {
-    val longestChar = hashSetOf<Char>()
-    var longestSize = 0
-
-    for (i in input.indices) {
-        for (j in i..input.lastIndex) {
-            if (longestChar.contains(input[j])) {
-                break
-            } else {
-                longestChar.add(input[j])
-            }
-        }
-
-        if (longestChar.size > longestSize) {
-            longestSize = longestChar.size
-        }
-
-        longestChar.clear()
-    }
-
-    return longestSize
-}
-
 private fun longestSubstring(input: String): Int {
-    val data = mutableMapOf<Char, Int>()
-    var longestSize = 0
-    var startIndex = 0
+    val lastIndex = mutableMapOf<Char, Int>()
+    var left = 0
+    var longestChar = 0
 
-    input.forEachIndexed { index, char ->
-        val indexChar = data[char]
+    for (right in input.indices) {
+        val char = input[right]
 
-        if (indexChar != null && indexChar >= startIndex) {
-            val possibleLongestSize = index - startIndex
-
-            if (possibleLongestSize > longestSize) {
-                longestSize = possibleLongestSize
-            }
-
-            startIndex = indexChar + 1
+        if (lastIndex.containsKey(char) && lastIndex[char]!! >= left) {
+            left = lastIndex[char]!! + 1
         }
 
-        data[char] = index
+        lastIndex[char] = right
+        longestChar = maxOf(longestChar, right - left + 1)
     }
 
-    val possibleLongestSize = input.length - startIndex
-    if (possibleLongestSize > longestSize) {
-        longestSize = possibleLongestSize
-    }
-
-    return longestSize
+    return longestChar
 }
 
 fun main() {
-
     //letters, digits, symbols and spaces
     val input = "abcabcbb"
     val longestSize = longestSubstring(input)
